5320e33bb195b30cfdc707b472c7506f5e3f827f
alexandrooliveira87 <eng.alexandrooliveira@gmail.com>
2024-04-10 10:52:44 -0300

    Criando a arquitetura do projeto

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 const express = require('express')
+const UserRoute = require("./src/routes/user.route")
 const app = express()
 
-//Rota
-    //Metodo HTTP - CRUD (Create, Read, Update, Delete)
-    //GET - Pega uma info
-    //POST - Cria uma info
-    //PUT - Altera toda info
-    //PATH - Altera parte da info
-    // Delete - Apaga uma info
-//Name - identificador da rota
-
-//Function (callback) - Responsável por executar algum comando
-app.get('/home', (req, res) => {
-  res.send('API de Teste')
-})
-
-.
-app.listen(3000)
+app.use("/soma", UserRoute);
+app.listen(3000);
