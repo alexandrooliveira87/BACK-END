51ff75b9f66f9422198ae65888ad50a8894eba92
alexandrooliveira87 <eng.alexandrooliveira@gmail.com>
2024-04-15 13:46:57 -0300

    Criando Update em contrução

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -57,6 +57,43 @@ const findById = async (req, res) => {
 
     res.send(user);
 
-}
+};
 
-module.exports = {create, findAll, findById};
+const update = async (req, res)=>{
+
+    const {name, username, email, password, avatar, background } = req.body;
+
+
+    if(!name && !username && !email && !password && !avatar && !background ){
+        res.status(400).send({mensage:"Submeta pelo menos um campo para fazer a atualização."});
+    }
+
+    const id = req.params.id;
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).send({massage: "usuário Inválido."});
+    }
+
+    const user = await userService.findByIdService(id);
+
+    if(!user){
+        return res.status(400).send({massage: "Usuário não encontrado."});
+    }
+
+    await userService.updateService(
+        id,
+        name, 
+        username,
+         email, 
+         password, 
+         avatar,
+          background
+    );
+
+    res.send({message:"Usuário foi atualizado com sucesso!"});
+
+
+
+
+};
+
+module.exports = {create, findAll, findById, update};
